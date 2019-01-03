438889300c2c56106ad7bb6acd85ea823b9935e5
shaoyuan <84977043@qq.com>
2019-01-03 16:14:54 +0800

    提交单元测试项目

---
+++ test/test.cpp
@@ -0,0 +1,13 @@
+﻿// test.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include "pch.h"
+#include <iostream>
+
+#include "gtest/include/gtest/gtest.h"
+
+int main(int argc, wchar_t* argv[]) {
+  testing::InitGoogleTest(&argc, argv);
+  RUN_ALL_TESTS();
+  return 0;
+}
