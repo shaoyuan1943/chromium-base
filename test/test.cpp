ec5dea97e0a3dc2ea2ff50af9b6d8bd838417187
shaoyuan <84977043@qq.com>
2019-03-18 11:50:28 +0800

    去掉本地单元测试

--- test/test.cpp
+++ test/test.cpp
@@ -4,16 +4,9 @@
 #include "pch.h"
 #include <iostream>
 
-#include "base/at_exit.h"
-#include "gtest/include/gtest/gtest.h"
 
 #pragma warning(disable:4251)
 
 int main(int argc, char* argv[]) {
-  base::AtExitManager exitMgr;
-
-
-  testing::InitGoogleTest(&argc, argv);
-  RUN_ALL_TESTS();
   return 0;
 }
